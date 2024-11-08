575e593cea47b8f1d1a84aa253f4020c94e26709
junaidmfaisal <junaidmfaisal@gmail.com>
2024-11-08 23:32:02 +0530

    v3

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-function toggleMenu(){
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+function toggleMenu() {
+    const mobileMenu = document.querySelector('.mobile-menu');
+    mobileMenu.classList.toggle('active'); // Toggle the 'active' class
 }
+
 
 // Function to check if an element is in the viewport
 function isInViewport(element) {
